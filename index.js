4687a3c857afc7376619aa6d98ec020eabd89472
Alex Luzhniy <83217719+tengu-alt@users.noreply.github.com>
2021-12-23 20:55:10 +0200

    sc

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 
-function registration() {
-    const emailInput = document.querySelector('.emailInput');
-    const passwordInput = document.querySelector('.passwordInput');
-    function validateEmail(email) {
-      const re = /^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]*$/;
-      return re.test(String(email).toLowerCase());
-    }
-    emailInput.addEventListener('input', function emailInputFunc() {
-      if (emailInput.validity.typeMismatch || !validateEmail(emailInput.value)) {
-        document.querySelector('.emailDiv').classList.remove('hidden');
-      } else {
-        document.querySelector('.emailDiv').classList.add('hidden');
-      }
-    });
-    passwordInput.addEventListener('input', function passwordInputFunc() {
-      document.querySelector('.passwordDiv').innerHTML = `${passwordInput.value.length} characters`;
-      if (passwordInput.value.length === 0) {
-        document.querySelector('.passwordDiv').innerHTML = '';
-      }
-    });
+function doResponse(){
+  const emailInput = document.querySelector('.emailInput');
+const passwordInput = document.querySelector('.passwordInput');
+const FnameInput = document.querySelector('.frNameInput');
+const LnameInput = document.querySelector('.lstNameInput');
+
+  const url = "http://127.0.0.1:8080/login";
+  const data = {
+    FirstName: FnameInput.value,
+    LastName: LnameInput.value,
+    email: emailInput.value,
+    password: passwordInput.value,
   }
+  const response = fetch(url, {
+    method: 'POST',
+    mode: 'cors', 
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow', 
+    referrerPolicy: 'no-referrer', 
+    body: JSON.stringify(data) 
+  });
+}
+
+function registration() {
+const sendBut = document.querySelector('.button')
+sendBut.addEventListener('click', doResponse);    
+}
   window.addEventListener('load', registration);
--- main.go
+++ main.go
@@ -2,43 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+func login(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	defer r.Body.Close()
+
+	fmt.Printf("%s", string(b))
+
+	http.ServeFile(w, r, "./index.html")
+}
 func hello(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
 	}
-
-	switch r.Method {
-	case "GET":
-		http.ServeFile(w, r, "./index.html")
-	case "POST":
-		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
-			return
-		}
-		//fmt.Fprintf(w, "activation link has been sent %v\n", r.PostForm)
-		fmt.Printf("Post from website! r.PostFrom = %v\n", r.PostForm)
-		Fname := r.FormValue("Fname")
-		Lname := r.FormValue("Lname")
-		email := r.FormValue("email")
-		password := r.FormValue("password")
-		fmt.Printf("First name = %s\n", Fname)
-		fmt.Printf("Last Name = %s\n", Lname)
-		fmt.Printf("Email = %s\n", email)
-		fmt.Printf("password = %s\n", password)
-		http.ServeFile(w, r, "./submit.html")
-	default:
-		fmt.Printf("Sorry, only GET and POST methods are supported.")
-	}
 }
 
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/login", login)
 	http.Handle("/index.js", http.FileServer(http.Dir("./")))
 	http.Handle("/eNWDJx.jpg", http.FileServer(http.Dir("./")))
 	http.Handle("/style.css", http.FileServer(http.Dir("./")))
